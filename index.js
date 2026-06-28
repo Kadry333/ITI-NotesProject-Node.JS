0e27cf00cc6d10427fef304a64db1d42712bcfa2
Kareem Kadry <cr7.rm35@gmail.com>
2026-06-28 20:04:44 +0300

    add swagger docs and readme file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 // app.use((req,res,next)=>{
@@ -10,9 +10,14 @@ app.use(cors());
 
 const notesRouter = require("./src/routes/notesRoutes");
 const authRouter = require("./src/routes/authRoutes");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-app.use("/notes",notesRouter);
-app.use("/auth",authRouter);
+app.use("/notes", notesRouter);
+app.use("/auth", authRouter);
 
+// Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
